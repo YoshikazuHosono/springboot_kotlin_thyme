0ffaca0352ccf33a96ddd324b79a9bfc41a0fea3
YoshikazuHosono <yyhhwork@gmail.com>
2020-07-24 18:24:11 +0900

    add list sample

--- src/main/kotlin/com/example/springboot_kotlin_thyme/controller/HelloController.kt
+++ src/main/kotlin/com/example/springboot_kotlin_thyme/controller/HelloController.kt
@@ -10,7 +10,19 @@ class HelloController {
     @GetMapping("/hello")
     fun hello(model: Model): String {
         model.addAttribute("message", "Hello hosono's thyme !!")
+
+        val list = listOf("Bon", "jo", "vi")
+        model.addAttribute("list", list)
+
+        val userList = listOf(User(1, "Bonjovi"), User(2, "Nonjovi"), User(3, "Sonjovi"))
+        model.addAttribute("userList", userList)
+
         return "hello"
     }
 
 }
+
+data class User(
+        val id: Long,
+        val name: String
+)
