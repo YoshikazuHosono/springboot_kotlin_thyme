a7d2f0845d6781709125a66f429c2284ddd07a5d
YoshikazuHosono <yyhhwork@gmail.com>
2020-07-24 17:48:30 +0900

    add

---
+++ src/main/kotlin/com/example/springboot_kotlin_thyme/SpringbootKotlinThymeleafApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springboot_kotlin_thyme
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinThymeleafApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinThymeleafApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springboot_kotlin_thyme/SpringbootKotlinThymeleafApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springboot_kotlin_thyme
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinThymeleafApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
